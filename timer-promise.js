92866ff61e6ff3479c26f52ef431c9b692609905
Julian Fechner <handballerj@hotmail.com>
2021-09-22 20:42:12 +0200

    renamed to timer-promise

--- timer-promise.js
+++ timer-promise.js
@@ -1,7 +1,7 @@
-function TimedPromise(executor,timeout=0){
+function TimerPromise(executor,timeout=0){
 	return Promise.race([
 		new Promise(executor),
 		new Promise((resolve,reject)=>!isNaN(parseInt(timeout)) && timeout > 0 ? setTimeout(()=>reject(new Error("Promise timedout after "+timeout+"ms")),timeout) : 0)
 	]);
 }
-module.exports = TimedPromise;
+module.exports = TimerPromise;
